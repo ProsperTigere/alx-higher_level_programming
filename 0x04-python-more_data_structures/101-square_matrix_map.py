1c1acc56082eace055f45873dd72dae52371274e
ProsperTigere <prospertigere2000@gmail.com>
2022-12-07 19:23:59 +0000

    task

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    keys_to_del = []
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
-            keys_to_del.append(key)
-    for key in keys_to_del:
-        del a_dictionary[key]
-    return a_dictionary
+def square_matrix_map(matrix=[]):
+    return list(map((lambda row: list(map((lambda x: x * x), row))), matrix))
+
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    for keys in sorted(a_dictionary.keys()):
-        print('{}: {}'. format(keys, a_dictionary[keys]))
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Priv
+def simple_delete(a_dictionary, key=""):
+    if key in a_dictionary:
+        del a_dictionary[key]
+    return a_dictionary
